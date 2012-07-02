a900047544334878c6b91a48bdf4f283a830105a
GeneralDon <generaldon@gmail.com>
2012-07-02 17:33:44 -0400

    New (buggy) Addition

---
+++ src/com/github/generaldon/Commands/Cmds/ClearAddiction.java
@@ -0,0 +1,47 @@
+package com.github.generaldon.Commands.Cmds;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.github.generaldon.Druggie;
+import com.github.generaldon.Commands.Main;
+import com.github.generaldon.Drugs.Full;
+import com.github.generaldon.General.ConfigManager;
+
+public class ClearAddiction extends Main{
+	
+	@Override
+	public void execute(CommandSender sender, String[] args)
+	{
+		private static dat = Full.getDAT;
+		Druggie.Addict.put(sender.getName(), Integer.valueOf(0));
+	      Player p = (Player)sender;
+	      p.sendMessage(Full.getDAT(0, 0));
+	}
+	
+	@Override
+	public String cmd() {
+		return "clean";
+	}
+
+	@Override
+	public String usage() {
+		return cmd();
+	}
+
+	@Override
+	public String description() {
+		return "shows help";
+	}
+
+	@Override
+	public String permission() {
+		return "Druggie.drug.help";
+	}
+
+	@Override
+	public int numArgs() {
+		return 0;
+	}
+
+}
