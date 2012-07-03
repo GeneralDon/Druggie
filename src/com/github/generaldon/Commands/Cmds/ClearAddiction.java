964a3b778a57b90153fb26ea2db51fb4a27b6ef5
GeneralDon <generaldon@gmail.com>
2012-07-03 16:56:47 -0400

    Took out unused imports

--- src/com/github/generaldon/Commands/Cmds/ClearAddiction.java
+++ src/com/github/generaldon/Commands/Cmds/ClearAddiction.java
@@ -5,19 +5,13 @@ import org.bukkit.entity.Player;
 
 import com.github.generaldon.Druggie;
 import com.github.generaldon.Commands.Main;
-import com.github.generaldon.Drugs.Full;
 import com.github.generaldon.General.ChatVariables;
-import com.github.generaldon.General.ConfigManager;
 
 public class ClearAddiction extends Main{
 	
 	@Override
 	public void execute(CommandSender sender, String[] args)
 	{
-<<<<<<< HEAD
-=======
-		private static dat = Full.getDAT; //Buggy right here, don't know how to do this piece.
->>>>>>> 18c69570b5a0600ae746b7f27003d18a8c3cc4ef
 		Druggie.Addict.put(sender.getName(), Integer.valueOf(0));
 	      Player p = (Player)sender;
 	      p.sendMessage(ChatVariables.LANGUAGE_DATA[0]);
